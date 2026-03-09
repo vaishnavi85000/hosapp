e39bf89c80bcca63dfd53bfc8b86eadf56fba288
vaishnavi85000 <thanneruvaishu8074@gmail.com>
2026-03-09 18:23:04 +0530

    'Auto-Sync'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { StatusBar, Style } from '@capacitor/status-bar'; // Import these
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor() { }
+
+  async ngOnInit() {
+    // This makes the top bar white on Android
+    await StatusBar.setBackgroundColor({ color: '#ffffff' });
+
+    // This makes the time/battery icons Dark (so they show on white)
+    await StatusBar.setStyle({ style: Style.Light });
+  }
 }
